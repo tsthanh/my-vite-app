1aedeb7442804939784b8a046d45662b42633c45
sonke98 <nhson260198@gmail.com>
2025-05-17 15:15:35 +0700

    Add GitHub Pages deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-vite-app/', // ⚠️ rất quan trọng
+  base: '/my-vite-app/', // 👈 repo name
   server: {
     proxy: {
       '/api': {
